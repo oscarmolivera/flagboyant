0c5a128425e214bc81782c0842325cd1fca305e6
Oscar M. Olivera <oscarmolivera@gmail.com>
2024-02-22 16:05:21 -0300

    [M1] Cleanning excess unnecessary code

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
-
-  get "/up/", to: "up#index", as: :up
-  get "/up/databases", to: "up#databases", as: :up_databases
+  root to: "game_identify_flags#index"
 
   # Sidekiq has a web dashboard which you can enable below. It's turned off by
   # default because you very likely wouldn't want this to be available to
@@ -10,8 +7,8 @@ Rails.application.routes.draw do
   #
   # Uncomment the 2 lines below to enable the dashboard WITHOUT authentication,
   # but be careful because even anonymous web visitors will be able to see it!
-  # require "sidekiq/web"
-  # mount Sidekiq::Web => "/sidekiq"
+  require "sidekiq/web"
+  mount Sidekiq::Web => "/sidekiq"
   #
   # If you add Devise to this project and happen to have an admin? attribute
   # on your user you can uncomment the 4 lines below to only allow access to
