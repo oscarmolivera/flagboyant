7fb39ac9d309457a002ac45415ad00880f6e7a8d
Oscar M. Olivera <oscarmolivera@gmail.com>
2024-02-22 19:51:06 -0300

    [M1] initial seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,24 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Base Countries.
+# Clear existing data
+Country.destroy_all
+
+# Seed data for countries
+countries = [
+  { name: "United States", code: "US", currency: "USD"},
+  { name: "Brazil", code: "BR", currency: "BRL"},
+  { name: "Mexico", code: "MX", currency: "MXN"},
+  { name: "Colombia", code: "CO", currency: "COP"},
+  { name: "Argentina", code: "AR", currency: "ARS"},
+  { name: "Canada", code: "CA", currency: "CAD"},
+  { name: "Peru", code: "PE", currency: "PEN"},
+  { name: "Venezuela", code: "VE", currency: "VES"},
+  { name: "Chile", code: "CL", currency: "CLP"},
+  { name: "Ecuador", code: "EC", currency: "USD"}
+]
+
+# Create countries
+countries.each do |country_data|
+  Country.create!(country_data)
+end
+
+puts "Countries seeded successfully!"
